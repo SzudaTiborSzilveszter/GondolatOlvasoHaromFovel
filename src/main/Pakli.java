bf9076206f2366de74a3524cb35bb62b2fd1ac75
SzudaTiborSzilveszter <szudatiborszilveszter@gmail.com>
2026-09-24 13:22:28 +0200

    kever metodus befejezve

--- src/main/Pakli.java
+++ src/main/Pakli.java
@@ -26,7 +26,33 @@ public class Pakli {
     public Lap[] getLapok() {
         return Arrays.copyOf(lapok, lapok.length);
     }
-    
+    public void kever(int valasztottOszlop) {
+        Lap[] ujPakli = new Lap[22];
+        switch (valasztottOszlop) {
+            case 1 -> {
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = lapok[20 - (i - 1) * 3];
+                    ujPakli[i + 7] = lapok[19 - (i - 1) * 3];
+                    ujPakli[i + 14] = lapok[21 - (i - 1) * 3];
+                }
+            }
+            case 2 -> {
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = lapok[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = lapok[20 - (i - 1) * 3];
+                    ujPakli[i + 14] = lapok[21 - (i - 1) * 3];
+                }
+            }
+            case 3 -> {
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = lapok[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = lapok[21 - (i - 1) * 3];
+                    ujPakli[i + 14] = lapok[20 - (i - 1) * 3];
+                }
+            }
+        }
+        this.lapok = ujPakli;
+    }
     public Lap ezVolt() {
         return lapok[11];
     }

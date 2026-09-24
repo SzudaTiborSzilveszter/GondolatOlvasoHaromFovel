fbad2c4547f0fe86e885200056b0837ebec0e5d1
gubek.veronika <gubek.veronika@DESKTOP-6SEMLRJ.akk.lan>
2026-09-24 13:19:14 +0200

    kirak metodus elkeszitve

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,9 +1,21 @@
 package main;
 
-public class Program {
+import java.util.Scanner;
 
+public class Program {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        Pakli pakli = new Pakli();
     }
-    
+    private static void kirak(Pakli pakli) {
+        Lap[] lapok = pakli.getLapok();
+        for (int i = 0; i < 7; i++) {
+            System.out.printf(
+                    "%-12s %-12s %-12s%n",
+                    lapok[i * 3 + 1].megj(),
+                    lapok[i * 3 + 2].megj(),
+                    lapok[i * 3 + 3].megj()
+            );
+        }
+    }
 }

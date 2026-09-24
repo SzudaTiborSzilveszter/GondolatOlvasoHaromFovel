f380ee83cbaa0ac0a43cbbea56f289ced59603eb
Ponauer Szilvia Mária <ponauer.szilvia@akk.lan>
2026-09-24 13:14:51 +0200

    Lap osztaly letrehozva

--- src/main/Lap.java
+++ src/main/Lap.java
@@ -1,5 +1,22 @@
 package main;
 
 public class Lap {
-    
+    private String ertek;
+    private String szin;
+
+    public Lap(String ertek, String szin) {
+        this.ertek = ertek;
+        this.szin = szin;
+    }
+
+    public String getErtek() {
+        return ertek;
+    }
+
+    public String getSzin() {
+        return szin;
+    }
+    public String megj() {
+        return szin + "_" + ertek;
+    }
 }

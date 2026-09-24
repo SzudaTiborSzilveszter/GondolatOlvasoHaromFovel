1c93487a263bbca3f798a5d03653d05029dfc550
gubek.veronika <gubek.veronika@DESKTOP-6SEMLRJ.akk.lan>
2026-09-24 15:19:01 +0200

    harom oszlop, oszlop erveynes, keveres tesztek

--- src/gondolotOlvasoHaromFovelTeszt/Teszt.java
+++ src/gondolotOlvasoHaromFovelTeszt/Teszt.java
@@ -11,6 +11,9 @@ public class Teszt {
         tesztLapokSzama();
         tesztNull();
         tesztAlahuzas();
+        tesztHaromOszlop();
+        tesztOszlopErvenyes();
+        tesztKeveres();
         System.out.println("Lap tesztek:");
         System.out.println("Minden teszt sikeres.");
     }
@@ -42,4 +45,46 @@ public class Teszt {
             assert lapok[i].megj().contains("_") : "A lap nevében nincs aláhúzás";
         }
     }    
+    
+      private static void tesztHaromOszlop() {
+        Pakli pakli = new Pakli();
+        Lap[] lapok = pakli.getLapok();
+        for (int sor = 0; sor < 7; sor++) {
+            int db = 0;
+            for (int oszlop = 1; oszlop <= 3; oszlop++) {
+                int index = sor * 3 + oszlop;
+                if (index < lapok.length && lapok[index] != null) {
+                    db++;
+                }
+            }
+            assert db == 3 : "Nem 3 lap van egy sorban";
+        }
+    }
+
+    private static void tesztOszlopErvenyes() {
+        assert oszlopErvenyes(1);
+        assert oszlopErvenyes(2);
+        assert oszlopErvenyes(3);
+        assert !oszlopErvenyes(0);
+        assert !oszlopErvenyes(4);
+        assert !oszlopErvenyes(-1);
+    }
+
+    private static boolean oszlopErvenyes(int oszlop) {
+        return oszlop >= 1 && oszlop <= 3;
+    }
+
+    private static void tesztKeveres() {
+        Pakli pakli = new Pakli();
+        Lap[] eredeti = pakli.getLapok().clone();
+        pakli.kever(1);
+        Lap[] kevert = pakli.getLapok();
+        boolean megvaltozott = false;
+        for (int i = 1; i < eredeti.length; i++) {
+            if (eredeti[i] != kevert[i]) {
+                megvaltozott = true;
+            }
+        }
+        assert megvaltozott : "A keverés nem változtatta meg a sorrendet";
+    }
 }

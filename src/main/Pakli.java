8738697ce1b3184f22c99f10f74e3b1de047f9cd
SzudaTiborSzilveszter <szudatiborszilveszter@gmail.com>
2026-09-24 13:18:11 +0200

    Lapok letrehozva, kever metodus nincs kesz

--- src/main/Pakli.java
+++ src/main/Pakli.java
@@ -1,5 +1,33 @@
 package main;
 
+import java.util.Arrays;
+
 public class Pakli {
+    protected Lap[] lapok;
+    public Pakli() {
+        this(21);
+    }
+    public Pakli(int db) {
+        this.lapok = new Lap[db + 1];
+        feltolt();
+    }
+    private void feltolt() {
+        String[] szinek = {"♠", "♣", "♥", "♦"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+        int db = 0;
+        for (String szin : szinek) {
+            for (String ertek : ertekek) {
+                if (db < 21) {
+                    lapok[++db] = new Lap(szin, ertek);
+                }
+            }
+        }
+    }
+    public Lap[] getLapok() {
+        return Arrays.copyOf(lapok, lapok.length);
+    }
     
+    public Lap ezVolt() {
+        return lapok[11];
+    }
 }
